04b27a7066347bf2ad0592b580b640a549e3799e
R1ma0 <cactus-2015@mail.ru>
2023-04-22 23:22:28 +0300

    [Segmentation fault] Implemented Zhang Suen method

--- src/zhang_suen.cpp
+++ src/zhang_suen.cpp
@@ -4,16 +4,109 @@ namespace zhang_suen_namespace
 {
 	void ZhangSuen::execute(cv::Mat & inImg)
 	{
-		int * neighbours {new int[8]};
+		unsigned int neighboursSize {8};
+		int * neighbours {new int[neighboursSize]};
 		int sumOfTransitions;
+		int sumOfNeighbours;
+		bool isStepOneChangesComplete {false};
+		bool isStepTwoChangesComplete {false};
+		std::forward_list<cv::Point2i> stepOneChanges;
+		std::forward_list<cv::Point2i> stepTwoChanges;
 		
 		replacePixelValue(inImg, 255, 1);
-		//extractPixelNeighbours(inImg, 1, 1, neighbours);
-		//extractSumOfTransitions(sumOfTransitions, neighbours);
+
+		while (!isStepOneChangesComplete || !isStepTwoChangesComplete)
+		{
+			stepOneChanges.clear();
+			stepTwoChanges.clear();
+
+			// Step one
+			for (int r = 1; r < inImg.rows - 1; r++)
+			{
+				for (int c = 1; c < inImg.cols - 1; c++)
+				{
+					extractPixelNeighbours(inImg, r, c, neighbours);
+					extractSumOfTransitions(sumOfTransitions, neighbours, neighboursSize);
+					sumOfNeighbours = getSumOfNeighbours(neighbours, neighboursSize);
+					
+					if (
+						inImg.at<uchar>(r, c) == 1 &&
+						sumOfNeighbours >= 2 && 
+						sumOfNeighbours <= 6 &&
+						sumOfTransitions == 1 &&
+						neighbours[0] * neighbours[2] * neighbours[4] == 0 &&
+						neighbours[2] * neighbours[4] * neighbours[6] == 0
+					)
+					{
+						stepOneChanges.insert_after(stepOneChanges.end(), cv::Point2i(r, c));
+					}
+				}
+			}
+
+			if (stepOneChanges.empty())
+			{
+				isStepOneChangesComplete = true;
+			}
+			else
+			{
+				for (cv::Point2i p : stepOneChanges)
+				{
+					inImg.at<uchar>(p) = 0;
+				}
+			}
+
+			// Step two
+			for (int r = 1; r < inImg.rows - 1; r++)
+			{
+				for (int c = 1; c < inImg.cols - 1; c++)
+				{
+					extractPixelNeighbours(inImg, r, c, neighbours);
+					extractSumOfTransitions(sumOfTransitions, neighbours, neighboursSize);
+					sumOfNeighbours = getSumOfNeighbours(neighbours, neighboursSize);
+
+					if (
+						inImg.at<uchar>(r, c) == 1 &&
+						sumOfNeighbours >= 2 && 
+						sumOfNeighbours <= 6 &&
+						sumOfTransitions == 1 &&
+						neighbours[0] * neighbours[2] * neighbours[6] == 0 &&
+						neighbours[0] * neighbours[4] * neighbours[6] == 0
+					)
+					{
+						stepTwoChanges.insert_after(stepTwoChanges.end(), cv::Point2i(r, c));
+					}
+				}
+			}
+
+			if (stepTwoChanges.empty())
+			{
+				isStepTwoChangesComplete = true;
+			}
+			else
+			{
+				for (cv::Point2i p : stepTwoChanges)
+				{
+					inImg.at<uchar>(p) = 0;
+				}
+			}
+		}
+		
 		replacePixelValue(inImg, 1, 255);
 		
 		delete [] neighbours;
 		neighbours = nullptr;
+	}
+
+	int ZhangSuen::getSumOfNeighbours(int * n, int size)
+	{
+		int sum = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			sum += n[i];
+		}
+
+		return sum;
 	}
 
 	void ZhangSuen::replacePixelValue(cv::Mat & inImg, uchar from, uchar to)
@@ -28,16 +121,16 @@ namespace zhang_suen_namespace
 		}
 	}
 
-	void ZhangSuen::extractSumOfTransitions(int & sum, int * n)
+	void ZhangSuen::extractSumOfTransitions(int & sum, int * n, int size)
 	{
 		sum = 0;
 
-		for (int i = 0; i < 7; i++)
+		for (int i = 0; i < size - 1; i++)
 		{
 			sum += (n[i] == 0 && n[i + 1] == 1) ? 1 : 0;
 		}
 
-		sum += (n[7] == 0 && n[0] == 1) ? 1 : 0;
+		sum += (n[size - 1] == 0 && n[0] == 1) ? 1 : 0;
 	}
 
 	void ZhangSuen::extractPixelNeighbours(cv::Mat & img, int r, int c, int * n)
--- src/zhang_suen.h
+++ src/zhang_suen.h
@@ -2,14 +2,16 @@
 #define ZHANG_SUEN_H
 
 #include <opencv2/opencv.hpp>
+#include <forward_list>
 
 namespace zhang_suen_namespace
 {
 	class ZhangSuen
 	{
 	private:
+		int getSumOfNeighbours(int *, int);
 		void replacePixelValue(cv::Mat &, uchar, uchar);
-		void extractSumOfTransitions(int &, int *);
+		void extractSumOfTransitions(int &, int *, int);
 		void extractPixelNeighbours(cv::Mat &, int, int, int *);
 	public:
 		void execute(cv::Mat &);
