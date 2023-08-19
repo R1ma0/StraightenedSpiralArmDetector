471c19f5b485011bd8bdf0b394c2a2f0ab57db5b
R1ma0 <cactus-2015@mail.ru>
2023-08-19 10:33:56 +0300

    wxWidgets is integrated

---
+++ src/app.cpp
@@ -0,0 +1,45 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+	public:
+		bool OnInit() override;
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+class MyFrame : public wxFrame
+{
+	public:
+		MyFrame();
+	private:
+		void OnExit(wxCommandEvent &event);
+};
+
+bool MyApp::OnInit()
+{
+	MyFrame *frame = new MyFrame();
+	frame->Show(true);
+	return true;
+}
+
+MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello world!")
+{
+	wxMenu *menuFile = new wxMenu;
+	menuFile->Append(wxID_EXIT);
+
+	wxMenuBar *menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+
+	SetMenuBar(menuBar);
+
+	CreateStatusBar();
+	SetStatusText("Hello world!!!");
+
+	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+
+void MyFrame::OnExit(wxCommandEvent &event)
+{
+	Close(true);
+}
