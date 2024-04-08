b7f74dcec8f95b8f166d1b17a8403230db5ccbb2
Damir <cactus-2015@mail.ru>
2024-04-08 17:08:05 +0300

    Renamed GBS checker

--- StraightSpiralArmsDetector/src/model/gauss_block_size_checker.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GBS_CHECKER_H
-#define GBS_CHECKER_H
-
-const int GBS_SPIN_DEFAULT_VALUE{ 3 };
-
-int CheckGBSValue(int, int*);
-
-#endif
---
+++ StraightSpiralArmsDetector/src/model/odd_even_checker.cpp
@@ -0,0 +1,31 @@
+#include "odd_even_checker.hpp"
+
+int CheckOddValue(int value, int* oldValue)
+{
+    int newValue = value;
+
+    if (value % 2 == 0)
+    {
+        if (value - *oldValue > 0) { newValue = value + 1; }
+        if (value - *oldValue < 0) { newValue = value - 1; }
+    }
+
+    *oldValue = newValue;
+
+    return newValue;
+}
+
+int CheckEvenValue(int value, int* oldValue)
+{
+    int newValue = value;
+
+    if (value % 2 == 1)
+    {
+        if (value - *oldValue > 0) { newValue = value + 1; }
+        if (value - *oldValue < 0) { newValue = value - 1; }
+    }
+
+    *oldValue = newValue;
+
+    return newValue;
+}
